dcd98f363eada20d90eec32aac397f2c0e7ad911
Fonchu-Fonchu <fonchu.e.venyuy@gmail.com>
2022-07-28 03:55:58 -0700

    palindromes

---
+++ palindromeName/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string revName = "";
+
+while(name == null||name.Length == 0 || name == "")
+{
+    Console.Write("\n Please Enter your name: ");
+    name = Console.ReadLine();
+}
+    for (int i = name.Length-1; i >= 0; i--)
+    {
+        revName = revName+name[i];
+    }
+    Console.Write("\n Reversing the letters of you name it gives: ");
+Console.WriteLine(revName);
+if(revName == name)
+{
+    Console.WriteLine("\n Your name is a palindrome👌😊");
+}
+else
+{
+    Console.WriteLine("\n Your name is not a palindrome😢😎");
+}
---
+++ palindromeNumber/ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class PalindromeNumbers
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number");
+        int num=int.Parse(Console.ReadLine()), sum = 0, temp, rem;
+        temp = num;  // Storing the original number in a temporal variable
+        while(num> 0)
+        {
+            rem = num% 10; //Gets the remainder each time the number is divided by 10
+            sum = (sum * 10) + rem; //Takes the sum, multiply by 10 and add the remainder to it
+            //Console.WriteLine(sum);
+            num = num/10; //Dividing the original number by 10
+        }
+        if (temp == sum) {
+            Console.WriteLine("The number is a palindrome");
+        }
+        else { Console.WriteLine("The number is not a palindrome"); }
+    }
+}
